23efe2a3dc3752067eec145e65a519a2ee1657c2
David Mateus <23423789+davidmateusreis@users.noreply.github.com>
2022-06-10 13:19:03 -0300

    Criação dos métodos GET e POST

--- src/main/java/com/david/myvideogamelist/controllers/GameController.java
+++ src/main/java/com/david/myvideogamelist/controllers/GameController.java
@@ -1,10 +1,13 @@
 package com.david.myvideogamelist.controllers;
 
+import com.david.myvideogamelist.models.Game;
 import com.david.myvideogamelist.repositories.GameRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class GameController {
@@ -17,5 +20,16 @@ public class GameController {
 
         model.addAttribute("gameList", gameRepository.findAll());
         return "games/index";
+    }
+
+    @GetMapping("/games/new")
+    public String newGame(@ModelAttribute("game") Game game) {
+        return "/games/form";
+    }
+
+    @PostMapping("/games/save")
+    public String addGame(@ModelAttribute("game") Game game) {
+        gameRepository.save(game);
+        return "redirect:/games";
     }
 }
