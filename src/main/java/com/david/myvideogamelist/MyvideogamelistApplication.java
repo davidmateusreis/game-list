8ada20ea8c789cf2dc2eaa658722bb327c760bca
David Mateus <23423789+davidmateusreis@users.noreply.github.com>
2022-06-10 09:58:14 -0300

    Início

---
+++ src/main/java/com/david/myvideogamelist/MyvideogamelistApplication.java
@@ -0,0 +1,13 @@
+package com.david.myvideogamelist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyvideogamelistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyvideogamelistApplication.class, args);
+	}
+
+}
